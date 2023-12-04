afbfd8966b23f389034688602c0607c215cd27ac
rukhshan23 <rukhshanharoon@hotmail.com>
2023-12-04 00:03:46 -0500

    ball-half

---
+++ Assets/BallScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        rb.AddForce(Vector2.up * 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Bottom"))
+        {
+            Debug.Log("Ball hit the bottom of the screen.");
+        }
+    }
+}
